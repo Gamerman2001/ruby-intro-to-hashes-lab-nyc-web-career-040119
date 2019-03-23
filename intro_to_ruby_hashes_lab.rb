e9967bce2c9c7fbce1bbe947464555cefd7b887a
Troy Gooden <tro12185@gmail.com>
2019-03-23 17:42:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,7 @@ def monopoly_with_fourth_tier
 	    :pieces => 4,
 	    :names => {
 	      :reading_railroad => { "mortgage_value" => "$100"}, 
-	      :pennsylvania_railroad =>{ "mortgage_value" => "$200",
+	      :pennsylvania_railroad =>{ "mortgage_value" => "$200"},
 	      :b_and_o_railroad => { "mortgage_value" => "$400"},
 	      :shortline => { "mortgage_value" => "$600"}
 	    },
@@ -63,7 +63,7 @@ def monopoly_with_fourth_tier
 	      :three_pieces_owned => 100,
 	      :four_pieces_owned => 200
 	    },
-	    }
+	    
 	  }
 	}
 
